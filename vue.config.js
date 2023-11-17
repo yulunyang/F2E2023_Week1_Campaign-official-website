fbbf9479678383d3fd3b4b8cd8fd08d6a6266ad6
crystal <yulunyangfrontend@gmail.com>
2023-11-17 16:32:31 +0800

    set

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/F2E_Week1_Campaign_website'
+  publicPath: '/F2E_2023_Week1/'
 })
